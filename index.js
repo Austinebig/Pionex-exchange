8cc027b85072181d7440a230896431afe624a8b1
Austine <nwaforaugustine820@gmail.com>
2024-12-28 13:09:11 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Toggle Navbar Menu on Small Screens
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('active'); // Toggle the hamburger 'X' animation
+});
